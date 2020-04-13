f3e53e2de0223ef430a8c51aee159b56a1ec2365
Christoph911 <Christoph.hoppe1@gmx.de>
2020-04-13 21:32:07 +0200

    create clustering

--- apps/Test.py
+++ apps/Test.py
@@ -10,10 +10,12 @@ import dash_table
 import plotly.graph_objs as go
 
 import pandas as pd
+from dash.exceptions import PreventUpdate
+from sklearn.cluster import KMeans
 
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],suppress_callback_exceptions=True)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 server = app.server
+
 
 # import dataset
 def parse_data(contents, filename):
@@ -32,42 +34,87 @@ def parse_data(contents, filename):
             'Fehler beim Dateiupload!'
         ])
     return df
+
 # dropdown options
-@app.callback(Output('opt-dropdown', 'options'),
+# TODO: Callbacks redundanter code. bessere Lösung?
+@app.callback(
+    [Output('opt-dropdownX', 'options'),
+     Output('opt-dropdownY', 'options')],
     [
         Input('upload', 'contents'),
         Input('upload', 'filename')
 
     ]
 )
-def update_date_dropdown(contents,filename):
-    options = []
-    if contents:
-        contents = contents[0]
-        filename = filename[0]
-        df = parse_data(contents,filename)
-
-        options = [{'label': col, 'value': col} for col in df.columns]
-    return options
-
-@app.callback(Output('opt-dropdownX', 'options'),
-    [
-        Input('upload', 'contents'),
-        Input('upload', 'filename')
-
-    ]
-)
-def update_date_dropdownX(contents,filename):
+def update_date_dropdown(contents, filename):
     optionsX = []
+    optionsY = []
     if contents:
         contents = contents[0]
         filename = filename[0]
-        df = parse_data(contents,filename)
+        df = parse_data(contents, filename)
 
         optionsX = [{'label': col, 'value': col} for col in df.columns]
-    return optionsX
+        optionsY = [{'label': col, 'value': col} for col in df.columns]
+    return optionsX, optionsY
+
 
 # output clustering
+
+@app.callback(
+    Output("cluster-graph", "figure"),
+    [Input('button', 'n_clicks')],
+
+    [State("opt-dropdownX", "value"),
+     State("opt-dropdownY", "value"),
+     State("cluster-count", "value"),
+     State('upload', 'contents'),
+     State('upload', 'filename')
+     ],
+)
+def make_graph(n_clicks, x, y, n_clusters, contents, filename):
+    layout = None
+    if contents is None or filename is None or x is None or y is None or n_clusters is None:
+        raise PreventUpdate
+
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+        contents = parse_data(contents, filename)
+
+        # minimal input validation, make sure there's at least one cluster
+        km = KMeans(n_clusters=max(n_clusters, 1))
+        df = contents.loc[:, [x, y]]
+        km.fit(df.values)
+        df["cluster"] = km.labels_
+
+        centers = km.cluster_centers_
+
+        data = [
+            go.Scatter(
+                x=df.loc[df.cluster == c, x],
+                y=df.loc[df.cluster == c, y],
+                mode="markers",
+                marker={"size": 8},
+                name="Cluster {}".format(c),
+            )
+            for c in range(n_clusters)
+        ]
+
+        data.append(
+            go.Scatter(
+                x=centers[:, 0],
+                y=centers[:, 1],
+                mode="markers",
+                marker={"color": "#000", "size": 12, "symbol": "diamond"},
+                name="Cluster centers",
+            )
+        )
+
+        layout = {"xaxis": {"title": x}, "yaxis": {"title": y}}
+
+    return go.Figure(data=data, layout=layout)
+
 
 # masterlayout
 app.layout = html.Div(
@@ -82,6 +129,9 @@ app.layout = html.Div(
                 dbc.Label("X variable"),
                 dcc.Dropdown(
                     id="opt-dropdownX",
+                    options=[
+                        {'label': '', 'value': ''}
+                    ],
                 ),
             ]
         ),
@@ -89,16 +139,23 @@ app.layout = html.Div(
             [
                 dbc.Label("Y variable"),
                 dcc.Dropdown(
-                    id="opt-dropdown",
+                    id="opt-dropdownY",
+                    options=[
+                        {'label': '', 'value': ''}
+                    ],
                 ),
             ]
         ),
-        #dcc.Graph("figure")
+        dbc.FormGroup(
+            [
+                dbc.Label("Anzahl Cluster"),
+                dbc.Input(id="cluster-count", type="number", value=3),
+            ]
+        ),
+        html.Button('Click Me', id='button'),
+        dcc.Graph(id="cluster-graph"),
     ]
 )
-
-
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
