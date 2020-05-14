2c3683b8bca5cd6ece5ad7149be4c84f249d6f14
Christoph911 <Christoph.hoppe1@gmx.de>
2020-05-14 12:11:16 +0200

    create confusion matrix for logistic regression model

--- callbacks/callbacks_logistic_regression.py
+++ callbacks/callbacks_logistic_regression.py
@@ -7,9 +7,10 @@ import plotly.graph_objs as go
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegressionCV
-from sklearn.metrics import recall_score, precision_score, f1_score
+from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix
 from sklearn.preprocessing import StandardScaler
-
+import plotly.figure_factory as ff
+import sklearn.metrics as metrics
 @app.callback(
     [Output('zielwert-opt-log', 'options'),
      Output('train-test-opt-log', 'options')],
@@ -60,21 +61,50 @@ def make_log_regression(n_clicks, df, y, train_test_size):
     precision = precision_score(Y_test, Y_pred, average='micro')
     f1 = f1_score(Y_test, Y_pred, average='micro')
 
-    fig = go.Figure(
-        data=[
-            go.Scatter(
-                x=Y_test,
-                y=Y_pred,
-                mode="markers",
-                marker={"size": 8}
-            )
-        ]
-    )
-    fig.update_layout(
-        xaxis_title='Actual ' + y,
-        yaxis_title='Predict ' + y,
-        template='plotly_white'
-    )
+    # fig = go.Figure(
+    #     data=[
+    #         go.Scatter(
+    #             x=Y_test,
+    #             y=Y_pred,
+    #             mode="markers",
+    #             marker={"size": 8}
+    #         )
+    #     ]
+    # )
+    # fig.update_layout(
+    #     xaxis_title='Actual ' + y,
+    #     yaxis_title='Predict ' + y,
+    #     template='plotly_white'
+    # )
+
+    # create confusion matrix
+    confusion_matrix = metrics.confusion_matrix(Y_test, Y_pred)
+    # convert results into int
+    confusion_matrix = confusion_matrix.astype(int)
+    # get target names
+    target_names = Y.unique()
+    # split target names by comma and return list
+    target_names = ' '.join(target_names).split()
+
+    # set variables for matrix
+    z = confusion_matrix
+    x = target_names
+    y = target_names
+
+    # change each element of z to type string for annotations
+    z_text = [[str(y) for y in x] for x in z]
+
+    # set up figure
+    fig = ff.create_annotated_heatmap(z, x=x, y=y, annotation_text=z_text, colorscale='Blues')
+
+    # add title and margin
+    fig.update_layout(title_text='Confusion matrix',
+                      margin=dict(t=50, l=200)
+                      )
+
+
+    # add colorbar
+    fig['data'][0]['showscale'] = True
 
     return dict(figure=fig)
 
@@ -90,8 +120,8 @@ def create_tab_content(active_tab, data):
             figure = dcc.Graph(figure=data["figure"])
             return figure
         elif active_tab == "tab-2-log-reg":
-            metrics = html.P(['Recall score:', str(recall.round(3)), html.Br(),
-                              "Precision Score: ", str(precision.round(3)), html.Br(),
+            metrics = html.P(['Recall score: ', str(recall.round(3)), html.Br(),
+                              'Precision Score: ', str(precision.round(3)), html.Br(),
                               'F1 Score: ', str(f1.round(3)), html.Br()
                               ])
             return metrics
