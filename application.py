e96d45a7febacce25aff128369fdcbe7b604451d
Christoph911 <Christoph.hoppe1@gmx.de>
2020-06-03 23:23:19 +0200

    updated requirements.txt

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from main import app
+from main import app, application
 from layouts.layout_start import layout_start
 from callbacks import *
 from layouts.layout_upload import layout_upload
@@ -68,4 +68,4 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    application.run(debug=False, port=8080)
--- callbacks/callbacks_linear_regression.py
+++ callbacks/callbacks_linear_regression.py
@@ -27,7 +27,7 @@ def get_target(df,dummy):
 
 
 
-# linear regression, return two figures, store figures in index.py
+# linear regression, return two figures, store figures in application.py
 @app.callback(
     [Output("store-figure-reg", "data"),
     Output('store-figure-feat','data')],
